0a4ed773a02fad2f2f4e0ab1fb7152e704141373
jelly <jellychena@sina.com>
2016-06-21 09:03:24 +0800

    SharePreference

---
+++ SharePreferenceTest/src/com/example/sharepreferencetest/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.sharepreferencetest;
+
+import android.R.bool;
+import android.R.integer;
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+
+public class MainActivity extends Activity {
+
+	private Button saveData;
+	private Button restoreData;
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        saveData = (Button)findViewById(R.id.save_data);
+        saveData.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				
+				SharedPreferences.Editor editor = getSharedPreferences("data", MODE_PRIVATE).edit();
+				editor.putString("name", "Tom");
+				editor.putInt("age", 28);
+				editor.putBoolean("married", false);
+				editor.commit();
+			}
+		});
+        
+        restoreData = (Button)findViewById(R.id.restore_data);
+        restoreData.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				SharedPreferences pref = getSharedPreferences("data", MODE_PRIVATE);
+				String name = pref.getString("name", "");
+				int age = pref.getInt("age", 0);
+				boolean married = pref.getBoolean("married", false);
+				Log.d("MainActivity", "name is " + name);
+				Log.d("MainActivity", "age is " + age);
+				Log.d("MainActivity", "married is " + married);
+			}
+		});
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
